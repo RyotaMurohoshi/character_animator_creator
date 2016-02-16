a6d8c21ff3510e9bf7c2884749f3ac7a7b5f567c
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-02-16 22:20:07 +0900

    added ScriptDiveder Example

---
+++ unity/Assets/Editor/Examples/SpriteDividerExample.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class SpriteDividerExample
+{
+    [MenuItem("Assets/CharacterAnimatorCreator/Divide Textures")]
+    public static void DivideImages()
+    {
+        int horizontalCount = 4;
+        int verticalCount = 4;
+
+        IEnumerable<Texture> targets = Selection.objects.OfType<Texture>();
+
+        if (!targets.Any())
+        {
+            Debug.LogWarning("Please selecting textures.");
+            return;
+        }
+
+        foreach (Texture target in targets)
+        {
+            string assetPath = AssetDatabase.GetAssetPath(target);
+            SpriteDivider.DividSprite(assetPath, horizontalCount, verticalCount);
+        }
+    }
+}
+
