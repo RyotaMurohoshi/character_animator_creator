7273a6616cae1fb646f22624625fb8f3bf51b965
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-02-17 00:13:58 +0900

    added SpriteAnimationClipCreator

---
+++ unity/Assets/Editor/Examples/SpriteAnimationClipCreatorExample.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SpriteAnimationClipCreatorExample
+{
+    [MenuItem("Assets/CharacterAnimatorCreator/Create AnimationClip")]
+    public static void CreateAnimationClip()
+    {
+        IEnumerable<Sprite> sprites = Selection.objects.OfType<Sprite>();
+        if (!sprites.Any())
+        {
+            Debug.LogWarning("Please selecting sprites.");
+            return;
+        }
+
+        List<SpriteKeyframeDefinition> spriteKeyframes = sprites
+            .OrderBy(it => it.name)
+            .Select((Sprite sprite, int index) => new SpriteKeyframeDefinition
+            {
+                Value = sprite,
+                Time = 0.1F * index,
+            })
+            .ToList();
+
+        SpriteAnimationClipDefinition definition = new SpriteAnimationClipDefinition
+        {
+            SpriteKeyframes = spriteKeyframes,
+            WrapMode = WrapMode.Loop,
+            IsLoop = true,
+            FrameRate = 60.0F,
+            ResulutPath = "Assets/Sample.anim",
+        };
+
+        SpriteAnimationClipCreator.CreateAnimationClip(definition);
+    }
+}
