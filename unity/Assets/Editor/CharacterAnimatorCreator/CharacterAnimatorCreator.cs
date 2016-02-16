bcadf701f840be60ea1958db5d8ca497a67843a5
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-02-17 02:06:27 +0900

    add parameter for duration

--- unity/Assets/Editor/CharacterAnimatorCreator/CharacterAnimatorCreator.cs
+++ unity/Assets/Editor/CharacterAnimatorCreator/CharacterAnimatorCreator.cs
@@ -88,56 +88,56 @@ public static class CharacterAnimatorCreator
         return new Dictionary<CharacterState, SpriteAnimationClipDefinition>{
             {
                 CharacterState.Default,
-                CreateSpriteAnimationClipDefinition (true, string.Format("{0}/Default.anim", outputDirectoryName), sprites[1][0], sprites[1][1], sprites[1][2], sprites[1][1])
+                CreateSpriteAnimationClipDefinition (true, string.Format("{0}/Default.anim", outputDirectoryName), 0.2F, sprites[1][0], sprites[1][1], sprites[1][2], sprites[1][1])
             },
             {
                 CharacterState.Dead,
-                CreateSpriteAnimationClipDefinition (false, string.Format("{0}/Dead.anim", outputDirectoryName), sprites[2][9], sprites[2][10],sprites[2][11])
+                CreateSpriteAnimationClipDefinition (false, string.Format("{0}/Dead.anim", outputDirectoryName), 0.2F, sprites[2][9], sprites[2][10],sprites[2][11])
             },
             {
                 CharacterState.Pinch,
-                CreateSpriteAnimationClipDefinition (true, string.Format("{0}/Pinch.anim", outputDirectoryName), sprites[1][6], sprites[1][7], sprites[1][8], sprites[1][7])
+                CreateSpriteAnimationClipDefinition (true, string.Format("{0}/Pinch.anim", outputDirectoryName), 0.2F, sprites[1][6], sprites[1][7], sprites[1][8], sprites[1][7])
             },
             {
                 CharacterState.Hit,
-                CreateSpriteAnimationClipDefinition (false, string.Format("{0}/Hit.anim", outputDirectoryName), sprites[1][3], sprites[1][4], sprites[1][5])
+                CreateSpriteAnimationClipDefinition (false, string.Format("{0}/Hit.anim", outputDirectoryName), 0.1F, sprites[1][3], sprites[1][4], sprites[1][5])
             },
             {
                 CharacterState.Attack,
-                CreateSpriteAnimationClipDefinition (false, string.Format("{0}/Attack.anim", outputDirectoryName), sprites[3][0], sprites[3][1], sprites[3][2])
+                CreateSpriteAnimationClipDefinition (false, string.Format("{0}/Attack.anim", outputDirectoryName), 0.1F, sprites[3][0], sprites[3][1], sprites[3][2])
             },
             {
                 CharacterState.Down,
-                CreateSpriteAnimationClipDefinition (true, string.Format("{0}/Down.anim", outputDirectoryName), sprites[0][0], sprites[0][1], sprites[0][2], sprites[0][1])
+                CreateSpriteAnimationClipDefinition (true, string.Format("{0}/Down.anim", outputDirectoryName), 0.2F, sprites[0][0], sprites[0][1], sprites[0][2], sprites[0][1])
             },
             {
                 CharacterState.Left,
-                CreateSpriteAnimationClipDefinition (true, string.Format("{0}/Left.anim", outputDirectoryName), sprites[0][3], sprites[0][4], sprites[0][5], sprites[0][3])
+                CreateSpriteAnimationClipDefinition (true, string.Format("{0}/Left.anim", outputDirectoryName), 0.2F, sprites[0][3], sprites[0][4], sprites[0][5], sprites[0][3])
             },
             {
                 CharacterState.Right,
-                CreateSpriteAnimationClipDefinition (true, string.Format("{0}/Right.anim", outputDirectoryName), sprites[0][6], sprites[0][7], sprites[0][8], sprites[0][7])
+                CreateSpriteAnimationClipDefinition (true, string.Format("{0}/Right.anim", outputDirectoryName), 0.2F, sprites[0][6], sprites[0][7], sprites[0][8], sprites[0][7])
             },
             {
                 CharacterState.Up,
-                CreateSpriteAnimationClipDefinition (true, string.Format("{0}/Up.anim", outputDirectoryName), sprites[0][9], sprites[0][10], sprites[0][11], sprites[0][10])
+                CreateSpriteAnimationClipDefinition (true, string.Format("{0}/Up.anim", outputDirectoryName), 0.2F, sprites[0][9], sprites[0][10], sprites[0][11], sprites[0][10])
             },
         };
     }
 
-    static SpriteAnimationClipDefinition CreateSpriteAnimationClipDefinition(bool isLoop, string resultPath, params Sprite[] sprites)
+    static SpriteAnimationClipDefinition CreateSpriteAnimationClipDefinition(bool isLoop, string resultPath, float frameDuration, params Sprite[] sprites)
     {
         List<SpriteKeyframeDefinition> spriteKeyframes = sprites
             .Select((sprite, index) => new SpriteKeyframeDefinition
             {
                 Value = sprite,
-                Time = 0.2F * index
+                Time = frameDuration * index
             }).ToList();
 
         return new SpriteAnimationClipDefinition
         {
             SpriteKeyframes = spriteKeyframes,
-            WrapMode = WrapMode.Loop,
+            WrapMode = isLoop ? WrapMode.Loop : WrapMode.Default,
             IsLoop = isLoop,
             FrameRate = 4F,
             ResulutPath = resultPath,
