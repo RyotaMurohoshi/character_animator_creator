b66015f131b872a8770b1011fa7fc5e157d8a237
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-02-17 00:31:59 +0900

    added SingleStateAnimatorControllerCreator

---
+++ unity/Assets/Editor/CharacterAnimatorCreator/SingleStateAnimatorControllerCreator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+
+public static class SingleStateAnimatorControllerCreator
+{
+    public static RuntimeAnimatorController CreateAnimatorController(SingleStateAnimatorControllerDefinition definition)
+    {
+        AnimatorController animatorController = AnimatorController.CreateAnimatorControllerAtPath(definition.ResulutPath);
+        AnimatorStateMachine stateMachine = animatorController.layers[0].stateMachine;
+
+        foreach (AnimationClip clip in definition.AnimationClipList)
+        {
+            AnimatorState state = stateMachine.AddState(clip.name);
+            state.motion = clip;
+        }
+
+        EditorUtility.SetDirty(animatorController);
+
+        return animatorController;
+    }
+}
