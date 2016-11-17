53364f966f15d2770beaf1c3ae6d52e3d44e8881
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-11-18 02:09:26 +0900

    create SimpleTransitionAnimatorControllerCreator

---
+++ unity/Assets/Editor/CharacterAnimatorCreator/SimpleTransitionAnimatorControllerCreator.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+using System.Linq;
+
+public static class SimpleTransitionAnimatorControllerCreator
+{
+    public static RuntimeAnimatorController CreateAnimatorController(SimpleTransitionAnimatorControllerDefinition definition)
+    {
+        AnimatorController animatorController = AnimatorController.CreateAnimatorControllerAtPath(definition.ResulutPath);
+        AnimatorControllerLayer layer = animatorController.layers.First();
+        layer.name = definition.LayerName;
+        AnimatorStateMachine stateMachine = layer.stateMachine;
+
+        AnimatorState defaultState = stateMachine.AddState("Default");
+        defaultState.motion = definition.DefaultAnimationClip;
+
+        AnimatorState pinchState = stateMachine.AddState("Pinch");
+        pinchState.motion = definition.PinchAnimationClip;
+
+        AnimatorState hitState = stateMachine.AddState("Hit");
+        hitState.motion = definition.HitAnimationClip;
+
+        AnimatorState walkState = stateMachine.AddState("Walk");
+        walkState.motion = definition.WalkAnimationClip;
+
+        AnimatorControllerParameter hpRateParameter = new AnimatorControllerParameter
+        {
+            name = "HpRate",
+            defaultFloat = 1.0F,
+            type = AnimatorControllerParameterType.Float
+        };
+        animatorController.AddParameter(hpRateParameter);
+
+        stateMachine.defaultState = defaultState;
+
+        {
+            AnimatorStateTransition defaultToPinchTransition = new AnimatorStateTransition
+            {
+                destinationState = pinchState,
+                hasExitTime = false,
+                duration = 0.0F,
+            };
+            defaultToPinchTransition.AddCondition(AnimatorConditionMode.Less, 0.3F, hpRateParameter.name);
+            defaultState.AddTransition(defaultToPinchTransition);
+        }
+
+        {
+            AnimatorStateTransition pinchToDefulatTransition = new AnimatorStateTransition
+            {
+                destinationState = defaultState,
+                hasExitTime = false,
+                duration = 0.0F,
+            };
+            pinchToDefulatTransition.AddCondition(AnimatorConditionMode.Greater, 0.3F, hpRateParameter.name);
+            pinchState.AddTransition(pinchToDefulatTransition);
+        }
+
+        {
+            AnimatorStateTransition transition = hitState.AddTransition(defaultState);
+            transition.hasExitTime = true;
+            transition.exitTime = 1.0F;
+            transition.duration = 0.0F;
+        }
+
+        EditorUtility.SetDirty(animatorController);
+
+        return animatorController;
+    }
+}
+
+public class SimpleTransitionAnimatorControllerDefinition
+{
+    public AnimationClip DefaultAnimationClip { get; set; }
+
+    public AnimationClip PinchAnimationClip { get; set; }
+
+    public AnimationClip HitAnimationClip { get; set; }
+
+    public AnimationClip WalkAnimationClip { get; set; }
+
+    public string LayerName { get; set; }
+
+    public string ResulutPath { get; set; }
+}
---
+++ unity/Assets/Editor/Examples/SimpleTransitionAnimatorControllerExample.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public static class SimpleTransitionAnimatorControllerExample
+{
+    [MenuItem("Assets/CharacterAnimatorCreator/Example/Simple Transition Create AnimatorController")]
+    public static void CreateAnimatorController()
+    {
+        var textures = Selection.objects.OfType<Texture>();
+        if (!textures.Any())
+        {
+            Debug.LogWarning("Please selecting texture.");
+            return;
+        }
+
+        var targetTexture = textures.First();
+
+        var assetPath = AssetDatabase.GetAssetPath(targetTexture);
+        SpriteDivider.DividSprite(assetPath, 3, 4);
+
+        var sprites = AssetDatabase
+            .LoadAllAssetRepresentationsAtPath(assetPath)
+            .OfType<Sprite>()
+            .ToList();
+
+        var definition = new SimpleTransitionAnimatorControllerDefinition
+        {
+            ResulutPath = "Assets/SimpleAnimator.controller",
+            LayerName = "Base Layer",
+            DefaultAnimationClip = CreateSpriteAnimationClip(true, "Default", 0.2F, sprites[0], sprites[1], sprites[2], sprites[1]),
+            PinchAnimationClip = CreateSpriteAnimationClip(true, "Pinch", 0.2F, sprites[6], sprites[7], sprites[8], sprites[7]),
+            HitAnimationClip = CreateSpriteAnimationClip(false, "Hit", 0.1F, sprites[3], sprites[4], sprites[5]),
+            WalkAnimationClip = CreateSpriteAnimationClip(true, "Walk", 0.2F, sprites[9], sprites[10], sprites[11], sprites[10])
+        };
+
+        var animatorController = SimpleTransitionAnimatorControllerCreator.CreateAnimatorController(definition);
+
+        AssetDatabase.AddObjectToAsset(definition.DefaultAnimationClip, animatorController);
+        AssetDatabase.AddObjectToAsset(definition.PinchAnimationClip, animatorController);
+        AssetDatabase.AddObjectToAsset(definition.HitAnimationClip, animatorController);
+        AssetDatabase.AddObjectToAsset(definition.WalkAnimationClip, animatorController);
+        AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(definition.DefaultAnimationClip));
+        AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(definition.PinchAnimationClip));
+        AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(definition.HitAnimationClip));
+        AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(definition.WalkAnimationClip));
+    }
+
+    static AnimationClip CreateSpriteAnimationClip(bool isLoop, string name, float frameDuration, params Sprite[] sprites)
+    {
+        var spriteKeyframes = sprites
+            .Select((sprite, index) => new SpriteKeyframeDefinition
+            {
+                Value = sprite,
+                Time = frameDuration * index
+            }).ToList();
+
+        var definition = new SpriteAnimationClipDefinition
+        {
+            SpriteKeyframes = spriteKeyframes,
+            WrapMode = isLoop ? WrapMode.Loop : WrapMode.Default,
+            IsLoop = isLoop,
+            FrameRate = 4F,
+            Name = name,
+        };
+
+        return SpriteAnimationClipCreator.ConvertToAnimationClip(definition);
+    }
+}
