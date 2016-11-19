5bf6470b239ef581d5ea5fbb2c4c80258886636e
RyotaMurohoshi <ryotamurohoshi@gmail.com>
2016-11-19 15:16:38 +0900

    fix SpriteDivider

--- unity/Assets/Editor/CharacterAnimatorCreator/SpriteDivider.cs
+++ unity/Assets/Editor/CharacterAnimatorCreator/SpriteDivider.cs
@@ -6,24 +6,21 @@ public static class SpriteDivider
 {
     public static void Execute(string texturePath, int horizontalCount, int verticalCount)
     {
-        var texture = AssetDatabase.LoadAssetAtPath(texturePath, typeof(Texture)) as Texture;
-        var pixelPerUnit = Mathf.Min(texture.width / horizontalCount, texture.height / verticalCount);
-
-        Execute(texturePath, horizontalCount, verticalCount, pixelPerUnit);
-    }
-
-    public static void Execute(string texturePath, int horizontalCount, int verticalCount, int pixelPerUnit)
-    {
         var importer = TextureImporter.GetAtPath(texturePath) as TextureImporter;
         importer.textureType = TextureImporterType.Sprite;
         importer.spriteImportMode = SpriteImportMode.Multiple;
         importer.filterMode = FilterMode.Point;
         importer.mipmapEnabled = false;
 
+        EditorUtility.SetDirty(importer);
+        AssetDatabase.ImportAsset(texturePath, ImportAssetOptions.ForceUpdate);
+
         Texture texture = AssetDatabase.LoadAssetAtPath(texturePath, typeof(Texture)) as Texture;
+        var pixelPerUnit = Mathf.Min(texture.width / horizontalCount, texture.height / verticalCount);
         importer.spritePixelsPerUnit = pixelPerUnit;
         importer.spritesheet = CreateSpriteMetaDataArray(texture, horizontalCount, verticalCount);
 
+        EditorUtility.SetDirty(importer);
         AssetDatabase.ImportAsset(texturePath, ImportAssetOptions.ForceUpdate);
     }
 
